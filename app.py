210b8daa2c61e1d5c631ff8d3f464e4835046c9f
Romane-henn <romanehennuyer6@gmail.com>
2026-02-11 16:05:27 +0100

    avancé

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Bienvenue sur ma page d'accueil ! <br><a href='http://127.0.0.1:5001/map'>Voir la carte</a>"
-
-# @app.route('/map')
-# def map():
-#     return render_template("map.html")
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5001)
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -32,16 +19,10 @@ def about():
 def projects():
     return render_template("projects.html")
 
-import os
-from flask import Flask, render_template, send_from_directory
-
-# ... ton code existant ...
-
 @app.route('/gallery')
 def gallery():
     images = os.listdir(os.path.join('static', 'images'))
     return render_template("gallery.html", images=images)
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
