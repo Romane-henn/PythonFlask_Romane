516fe281346b421ff1ace79eb3b87acd18a63d42
Romane-henn <romanehennuyer6@gmail.com>
2026-02-11 14:57:40 +0100

    Ajout mise en forme, menu déroulant, pages about/projects et carte ULCO Calais

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Bienvenue sur ma page d'accueil ! <br><a href='http://127.0.0.1:5001/map'>Voir la carte</a>"
+
+# @app.route('/map')
+# def map():
+#     return render_template("map.html")
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur ma page d'accueil ! <br><a href='/map'>Voir la carte</a>"
+    return render_template("index.html")
 
 @app.route('/map')
 def map():
     return render_template("map.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
