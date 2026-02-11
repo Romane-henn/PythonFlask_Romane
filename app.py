53186c2df324bc6f8e2dc977c7fd7a8c474986d1
Romane-henn <romanehennuyer6@gmail.com>
2026-02-11 14:06:17 +0100

    Ajout de la page carte avec Leaflet

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return "Bienvenue sur ma page d'accueil ! <br><a href='/map'>Voir la carte</a>"
 
-@app.route('/about')
-def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+@app.route('/map')
+def map():
+    return render_template("map.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
